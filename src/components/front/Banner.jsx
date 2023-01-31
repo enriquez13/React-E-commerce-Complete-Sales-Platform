6f942b793b07fd90447046aef52dbf16aab49167
Alejandro <alejo.enriquez@hotmail.com>
2023-01-30 21:52:39 -0500

    banner

--- src/components/front/Banner.jsx
+++ src/components/front/Banner.jsx
@@ -15,23 +15,26 @@ export const Banner = () => {
     {url:"https://i.pinimg.com/564x/b1/58/22/b15822678168c01505d0b91d06edc80f.jpg"},
  
    ]
-  
+
+
+   const[verificacion, SetVerificacion] = useState(true) 
   const [currentIndex,setCurrentIndex] = useState(0) 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex)  
   }
-
-
-
   const nextSlide = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex) 
+    
   }
-
-setTimeout(nextSlide, 4000)
+ 
+  verificacion === true ? setTimeout( 
+    nextSlide , 3000) : "" 
+ 
+  
 
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex)
@@ -39,11 +42,17 @@ setTimeout(nextSlide, 4000)
   return (
     <>
 
-<div className='mt-[2.5rem]  md:mt-[4.5rem] max-w-[1400px] h-[36rem] w-full md:max-h-[500px]  m-auto  relative group text-gray-100'>
-  <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full bg-center bg-cover transition duration-700'></div>
+<div className='mt-[2.5rem]  md:mt-[4.5rem] max-w-[1400px] h-[30.2rem] w-full md:max-h-[500px]  m-auto  relative group text-gray-100'>
+  <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full bg-center bg-cover '></div>
   
   <div onClick={prevSlide} className='md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-3xl  rounded-full p-2 '><BsChevronCompactLeft /></div>
-  <div onClick={nextSlide} className='md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-3xl rounded-full p-2'><BsChevronCompactRight /></div>
+  <div onClick={nextSlide} className='md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-3xl rounded-full p-2 '><BsChevronCompactRight /></div>
+  <div  className='w-full md:hidden md:group-hover:block absolute top-[50%]  translate-y-[-50%] text-center text-2xl rounded-lg py-3 
+   hover:bg-gray-200 hover:text-black font-extrabold tracking-wider text-gray-200'>Mens´s Shop</div>
+  <div  className='md:hidden md:group-hover:block absolute top-[62%] -translate-x-0 translate-y-[-50%] right-[28%] 
+  text-xl rounded-lg py-2 border px-10 hover:bg-gray-200 hover:text-black font-extrabold tracking-wider 
+  text-gray-200'>Collection</div>
+  
     <div className='flex absolute top-[88%] w-full justify-center '>
     {slides.map((slide, slideIndex) =>(
       <div key={slideIndex} onClick={()=> goToSlide(slideIndex)} className={` ${slideIndex == currentIndex ? "text-[0.8rem] mt-[0.29rem] mb-[0.29rem] mx-2 ": "text-[1.4rem]"} mx-2 cursor-pointer`}>
