c1c9172d6717fb4d0d6a3ceaf67bcb6a039be8d0
Alejandro <alejo.enriquez@hotmail.com>
2023-02-11 22:48:29 -0500

    navegacion-Top

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,35 @@ import {Pay} from './components/Pay'
 import {Cart} from './components/Cart'
 import {Admin} from '../src/components/Sesiones/Admin/Admin'
 import { Sesion } from './components/Sesiones/Sesion'
+import { useLocation } from 'react-router-dom';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 function App() {
-  
   return (
-<div className='bg-[#FFFEFB] text-black '>
+    <div className='bg-[#FFFEFB] text-black '>
     <Router>
-      <CartProvider>
-        <Routes>
-          <Route path='/*' element={<Home />} />
-          <Route path='/products' element={<ItemListContainer />} />
-          <Route path='/categoria/:categoriaId' element={<ItemListContainer />} />
-          <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/pay' element={<Pay />} />
-          <Route path='/sesion' element={<Sesion />} />
-          <Route path='/admin' element={<Admin />} />
-        </Routes>
-      </CartProvider>
+      <ScrollToTop />
+        <CartProvider>
+          <Routes>
+            <Route path='/*' element={<Home />} />
+            <Route path='/products' element={<ItemListContainer />} />
+            <Route path='/categoria/:categoriaId' element={<ItemListContainer />} />
+            <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/pay' element={<Pay />} />
+            <Route path='/sesion' element={<Sesion />} />
+            <Route path='/admin' element={<Admin />} />
+          </Routes>
+        </CartProvider>
     </Router>
     </div>
-    )
+  )
 }
 export default App
