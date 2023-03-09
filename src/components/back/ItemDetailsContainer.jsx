9eb59b29f83ecf2358d7366f6563923e7e8f9857
Alejandro <alejo.enriquez@hotmail.com>
2023-03-09 15:06:59 -0500

    new-product

--- src/components/back/ItemDetailsContainer.jsx
+++ src/components/back/ItemDetailsContainer.jsx
@@ -46,7 +46,6 @@ export const ItemDetailContainer = ()=>{
         <>
         <Navbar />
         <ItemDetail data={data} allProducts={allProducts}/>
-        
         <ItemList />
       
         </>
