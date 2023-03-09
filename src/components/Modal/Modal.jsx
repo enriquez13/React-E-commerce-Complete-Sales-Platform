89ef1c00bae21674132dc5956413d4bfd4b98e51
Alejandro <alejo.enriquez@hotmail.com>
2023-03-09 16:49:56 -0500

    col

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -120,7 +120,7 @@ export const Modal = (props) => {
                                     <div className=" col-span-2">
                                         <img
                                             src={ selectedProduct.id === producto.id && selectedImagen ? selectedImagen : producto.sizes[0].colors[0].imagen }
-                                            className="max-y-full object-cover"
+                                            className="max-w-[4rem] max-y-[4rem]"
                                         />
                                     </div>
                                
