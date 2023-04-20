f130b03653367c557aa6482b3d2f0f20e5e0191e
Alejandro <alejo.enriquez@hotmail.com>
2023-04-20 16:25:00 -0500

    link

--- src/components/Page/Contactanos.jsx
+++ src/components/Page/Contactanos.jsx
@@ -6,15 +6,22 @@ const Contactanos = () => {
   return (
     <>
       <NavBar />
-      <div className="mt-[5rem] text-center">
+      <div className="mt-[5rem] md:mt-[9rem] text-center">
         <h2 className="text-2xl font-bold">Contáctanos</h2>
       </div>
       <div className="mx-8 my-10">
         <p className="text-lg leading-7">
-          En nuestra tienda en línea, nos esforzamos por brindarte la mejor experiencia de compra posible. Es por ello que nuestro equipo de atención al cliente está siempre a tu disposición para responder tus dudas, preguntas y consultas.
+          En nuestra tienda en línea, nos esforzamos por brindarte la mejor experiencia de compra posible. 
+          Es por ello que nuestro equipo de atención al cliente está siempre a tu disposición para responder tus dudas, 
+          preguntas y consultas.
           <br />
           <br />
-          Si necesitas ponerte en contacto con nosotros, puedes hacerlo a través de correo electrónico a zorobabelcol@gmail.com. Nuestra respuesta es muy rápida y estaremos encantados de ayudarte en lo que necesites, desde preguntas sobre nuestros productos y servicios, hasta asesoría en tu proceso de compra o soporte post-venta.
+          Si necesitas ponerte en contacto con nosotros, puedes hacerlo a través de correo electrónico a 
+          <a href="mailto:zorobabelcol@gmail.com" className='text-blue-500'> zorobabelcol@gmail.com</a> o a nuestro teléfono o whatsapp 
+          <a href="https://wa.me/573127296362" className='text-green-500 hover:underline'> +573127296362</a>. 
+          Nuestra respuesta es muy rápida y estaremos encantados de ayudarte en lo que necesites,
+           desde preguntas sobre nuestros productos y servicios, hasta asesoría en tu proceso de compra o soporte 
+           post-venta.
           <br />
           <br />
           También puedes encontrarnos en nuestras redes sociales, Facebooj, instagram o whatsapp donde publicamos contenido interesante y promociones exclusivas para nuestros seguidores. Síguenos en Instagram, Facebook o Twitter para estar al tanto de nuestras novedades y eventos.
